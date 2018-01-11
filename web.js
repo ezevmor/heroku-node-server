5ba00f05e4038484e9ba5ec4c430c82f4905c953
Elio Zevallos More <e.zevallos-ext@dominion.dominion.es>
2018-01-11 13:45:19 +0100

    agregada carpeta dist

--- web.js
+++ web.js
@@ -1,9 +1,7 @@
 var gzippo = require('gzippo');
 var express = require('express');
-var morgan = require('morgan');
 var app = express();
 
-//app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
 app.use(function(req, res){
